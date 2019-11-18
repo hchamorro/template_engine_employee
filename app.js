09576b543bb356a30fded0e81ce64910211fa240
Hannah Chamorro <hannah.chamorro@gmail.com>
2019-11-17 20:24:59 -0600

    prompts for manager

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Manager = require("./Manager");
 const questions = require("./questions");
 
 const employeeType = questions.employeeType;
+const managerQuestions = questions.managerQuestions;
 
 //make team members array of objects
 const prompt = inquirer.createPromptModule();
@@ -16,10 +17,16 @@ teamMembers.push(sam);
 
 //make a function to create team "team member type question inquq" then run set of questions assoctiated with the info needed for each class
 prompt(employeeType).then(({ employee }) => {
-  if (employee === "Manager") {
+  if (employee === "manager") {
+    managerQuestionaire();
   }
 });
 
+const managerQuestionaire = function() {
+  prompt(managerQuestions).then(answers => {
+    console.log(answers);
+  });
+};
 //if user chose engineer call engineer inq and engineer func to be built
 
 // then ({info}) to pplot new Engineer and make html
--- questions.js
+++ questions.js
@@ -8,4 +8,31 @@ const employeeType = {
   }
 };
 
-module.exports = { employeeType };
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Full Name: "
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "ID Number: ",
+    validate: function(value) {
+      var valid = !isNaN(value);
+      return valid || "Please enter a number";
+    }
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email Address: "
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Office Number: "
+  }
+];
+
+module.exports = { employeeType, managerQuestions };
